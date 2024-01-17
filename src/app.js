fce91bef94e7252240d180a9d5aa7a63fea2b1f7
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2024-01-17 20:35:25 +0400

    Updated radio and checkbox

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import "./style.scss";
 
 /* const's */
 // const form header
-const hintSearch = document.querySelector("#hint");
+const hintSearch = document.querySelector(".hint");
 const mainHeaderSearch = document.querySelector(".main-header-search");
 const searchInputHeader = document.querySelector(".main-header-search-input");
 const btnCloseSearch = document.querySelector("#btn-close-search");
